9e4aeeafdb01965e64def275ad922b2cff493ae1
Sean Zheng <blackhorseya@gmail.com>
2024-03-30 18:45:30 +0800

    refactor: refactor access counter constructor and methods
    - Refactor `main()` function to `NewAccessCounter()` constructor
    - Add `Visit` and `GetVisit` methods to `AccessCounter` type
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- concurrent-map/main.go
+++ concurrent-map/main.go
@@ -5,6 +5,24 @@ type AccessCounter struct {
 	visits map[string]int
 }
 
-func main() {
+// NewAccessCounter is a constructor that returns a new AccessCounter.
+func NewAccessCounter() *AccessCounter {
+	return &AccessCounter{
+		visits: make(map[string]int),
+	}
+}
 
+// Visit is a method that increments the access counter for the given username.
+func (c *AccessCounter) Visit(username string) {
+	// todo: 2024/3/30|sean|Implement the Visit method
+	panic("implement me")
+}
+
+// GetVisit is a method that returns the access counter for the given username.
+func (c *AccessCounter) GetVisit(username string) int {
+	// todo: 2024/3/30|sean|Implement the GetVisit method
+	panic("implement me")
+}
+
+func main() {
 }
