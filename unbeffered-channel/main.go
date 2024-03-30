d6581b5a3104e339cf4e8cb8525033ded725d6f2
Sean Zheng <blackhorseya@gmail.com>
2024-03-30 12:45:06 +0800

    feat: implement ping-pong messaging functionality
    - Add a constant `interval` with a value of `3 * time.Second`
    - Create a `producer` function to send `ping` messages on a ticker interval
    - Create a `consumer` function to reply with `pong` when receiving `ping` messages
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- unbeffered-channel/main.go
+++ unbeffered-channel/main.go
@@ -1,4 +1,50 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	interval = 3 * time.Second
+)
+
 func main() {
+	ch := make(chan string)
+
+	go producer(ch)
+	go consumer(ch)
+
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		log.Println("signal received:", sig)
+
+		close(ch)
+	}
+}
+
+func producer(ch chan<- string) {
+	ticker := time.NewTicker(interval)
+
+	for {
+		select {
+		case <-ticker.C:
+			ch <- "ping"
+			log.Println("send ping")
+		}
+	}
+}
+
+func consumer(ch <-chan string) {
+	for s := range ch {
+		if s == "ping" {
+			log.Println("reply pong")
+		}
+	}
 }
