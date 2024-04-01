881ff93743e1b443a37d3f608f347afcb83fa240
Sean Zheng <blackhorseya@gmail.com>
2024-04-02 05:23:11 +0800

    fix: refactor worker management and execution logic
    - Update `job.go` to pass `workerID` to `Execute` method
    - Fix typo in `worker.go` file for `Worker` struct `id` field
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- complex-workerpool/job.go
+++ complex-workerpool/job.go
@@ -1,10 +1,30 @@
 package main
 
+import (
+	"log"
+)
+
 // Job is an interface that represents a job.
 type Job interface {
 	// Execute runs the task.
-	Execute() error
+	Execute(workerID int) error
 
 	// OnFailure is called when the task fails.
 	OnFailure(err error)
+}
+
+var _ Job = (*simpleJob)(nil)
+
+type simpleJob struct {
+	id int
+}
+
+func (j *simpleJob) Execute(workerID int) error {
+	log.Printf("worker %d is executing job %d", workerID, j.id)
+	return nil
+}
+
+func (j *simpleJob) OnFailure(err error) {
+	// TODO implement me
+	panic("implement me")
 }
--- complex-workerpool/woker.go
+++ complex-workerpool/woker.go
@@ -2,14 +2,14 @@ package main
 
 // Worker is a struct that represents a worker.
 type Worker struct {
-	ID       int
+	id       int
 	jobQueue chan Job
 }
 
 // NewWorker creates a new worker.
 func NewWorker(id int, jobQueue chan Job) *Worker {
 	return &Worker{
-		ID:       id,
+		id:       id,
 		jobQueue: jobQueue,
 	}
 }
@@ -18,7 +18,7 @@ func NewWorker(id int, jobQueue chan Job) *Worker {
 func (w *Worker) Run() {
 	go func() {
 		for job := range w.jobQueue {
-			err := job.Execute()
+			err := job.Execute(w.id)
 			if err != nil {
 				job.OnFailure(err)
 			}
