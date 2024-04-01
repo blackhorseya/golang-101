435080117e7268b43a8aadf4584cb68169141dac
Sean Zheng <blackhorseya@gmail.com>
2024-04-02 04:23:59 +0800

    feat: improve logging and structure in Job execution
    - Add logging to the `Job` struct and `Execute` method
    - Increase the `Job` struct definition from lines 11-15
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- workerpool/job.go
+++ workerpool/job.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"log"
+)
+
 // Job represents the job to be run.
 type Job struct {
 	ID int
@@ -10,4 +14,9 @@ func NewJob(id int) *Job {
 	return &Job{
 		ID: id,
 	}
+}
+
+// Execute runs the job.
+func (j *Job) Execute() {
+	log.Printf("Job %d is running\n", j.ID)
 }
