5313cc589d018cc68ee4f9c882443ebfce7258e8
Sean Zheng <blackhorseya@gmail.com>
2024-04-02 04:27:36 +0800

    refactor: refactor workerpool package into simple-workerpool package
    - Rename the `workerpool/job.go` file to `simple-workerpool/job.go`
    - Rename the `workerpool/main.go` file to `simple-workerpool/main.go`
    - Rename the `workerpool/pool.go` file to `simple-workerpool/pool.go`
    - Update the `workerpool/woker.go` file to `simple-workerpool/woker.go`
    - Add a `jobQueue` field to the `Worker` struct in the `simple-workerpool/woker.go` file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- simple-workerpool/woker.go
+++ simple-workerpool/woker.go
@@ -2,12 +2,14 @@ package main
 
 // Worker represents the worker that executes the job.
 type Worker struct {
-	ID int
+	ID       int
+	jobQueue chan *Job
 }
 
 // NewWorker creates a new worker with the given id.
 func NewWorker(id int) *Worker {
 	return &Worker{
-		ID: id,
+		ID:       id,
+		jobQueue: make(chan *Job),
 	}
 }
