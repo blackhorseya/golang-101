049785e556ec112478fcf875551768b376806129
Sean Zheng <blackhorseya@gmail.com>
2024-04-02 04:28:25 +0800

    chore: improve struct initialization and error handling
    - Remove unnecessary initialization of workers and jobs in Pool struct
    - Add a todo comment for implementing a feature with a specific deadline
    - Replace returning a new pool with a panic statement to indicate the feature is not implemented yet
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- simple-workerpool/pool.go
+++ simple-workerpool/pool.go
@@ -3,21 +3,10 @@ package main
 // Pool represents a pool of workers that can execute jobs.
 type Pool struct {
 	workerNums int
-	workers    []*Worker
-	jobs       []*Job
 }
 
 // NewPool creates a new pool of workers.
 func NewPool(workerNums int) *Pool {
-	pool := &Pool{
-		workerNums: workerNums,
-		workers:    make([]*Worker, workerNums),
-		jobs:       make([]*Job, 0),
-	}
-
-	for i := 0; i < workerNums; i++ {
-		pool.workers[i] = NewWorker(i)
-	}
-
-	return pool
+	// todo: 2024/4/2|sean|implement me
+	panic("implement me")
 }
