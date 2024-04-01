95177943bc68c15a6b1214a384c49a5dc13e7446
Sean Zheng <blackhorseya@gmail.com>
2024-04-02 04:48:29 +0800

    feat: refactor Task interface implementation
    - Add `OnFailure` method to the `Task` interface
    - Implement the `Execute` method in the `Task` interface
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- complex-workerpool/task.go
+++ complex-workerpool/task.go
@@ -1,1 +1,10 @@
 package main
+
+// Task is an interface that all tasks must implement.
+type Task interface {
+	// Execute runs the task.
+	Execute() error
+
+	// OnFailure is called when the task fails.
+	OnFailure(err error)
+}
