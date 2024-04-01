551ebbee14a54fea76e2df61160f788392adb5ed
Sean Zheng <blackhorseya@gmail.com>
2024-04-02 04:32:32 +0800

    feat: refactor pool struct to manage job queue efficiently
    - Add a jobsQueue field to the Pool struct
    - Implement the NewPool function to initialize the Pool struct with the jobsQueue field
    - Add a Start method to the Pool struct to start the pool of workers
    - Add an AddJob method to the Pool struct to add a job to the pool
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- simple-workerpool/pool.go
+++ simple-workerpool/pool.go
@@ -3,10 +3,26 @@ package main
 // Pool represents a pool of workers that can execute jobs.
 type Pool struct {
 	workerNums int
+	jobsQueue  chan *Job
 }
 
 // NewPool creates a new pool of workers.
 func NewPool(workerNums int) *Pool {
-	// todo: 2024/4/2|sean|implement me
-	panic("implement me")
+	return &Pool{
+		workerNums: workerNums,
+		jobsQueue:  make(chan *Job),
+	}
+}
+
+// Start starts the pool of workers.
+func (p *Pool) Start() {
+	for i := 0; i < p.workerNums; i++ {
+		worker := NewWorker(i, p.jobsQueue)
+		worker.Run()
+	}
+}
+
+// AddJob adds a job to the pool.
+func (p *Pool) AddJob(job *Job) {
+	p.jobsQueue <- job
 }
