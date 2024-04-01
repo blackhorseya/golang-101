28fe2a91743f404cd0d0402b3c391d008a74d598
Sean Zheng <blackhorseya@gmail.com>
2024-04-02 04:56:56 +0800

    feat: refactor Pool interface with new methods
    - Add `Start` method to the `Pool` interface
    - Add `Stop` method to the `Pool` interface
    - Add `SubmitJob` method to the `Pool` interface
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- complex-workerpool/pool.go
+++ complex-workerpool/pool.go
@@ -1,1 +1,13 @@
 package main
+
+// Pool represents a pool of workers.
+type Pool interface {
+	// Start starts the pool.
+	Start()
+
+	// Stop stops the pool.
+	Stop()
+
+	// SubmitJob submits a job to the pool.
+	SubmitJob(job Job)
+}
