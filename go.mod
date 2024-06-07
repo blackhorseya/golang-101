6ab00b0fa111275baa2da7cad9356b5c1d63bb8f
Sean Zheng <blackhorseya@gmail.com>
2024-06-07 23:48:54 +0800

    chore: implement OpenTelemetry tracing in Go application
    - Add OpenTelemetry dependencies to `go.mod`
    - Add a `simple-opentelemetry` Bazel build file
    - Add Go code for `simple-opentelemetry` application
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/blackhorseya/golang-101
 
 go 1.22.1
+
+require (
+	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.27.0
+	go.opentelemetry.io/otel/sdk v1.27.0
+)
+
+require (
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
---
+++ simple-opentelemetry/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+)
+
+func initTracer() func() {
+	exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	if err != nil {
+		panic(err)
+	}
+
+	tracer := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("simple-opentelemetry"),
+		)),
+	)
+	otel.SetTracerProvider(tracer)
+
+	return func() {
+		_ = tracer.Shutdown(context.Background())
+	}
+}
+
+func main() {
+	cleanup := initTracer()
+	defer cleanup()
+
+	tracer := otel.Tracer("simple-opentelemetry")
+
+	ctx, span := tracer.Start(context.Background(), "main")
+	defer span.End()
+
+	fmt.Println("Hello, OpenTelemetry!")
+	doWork(ctx)
+}
+
+func doWork(ctx context.Context) {
+	tracer := otel.Tracer("simple-opentelemetry")
+	_, span := tracer.Start(ctx, "doWork")
+	defer span.End()
+
+	// random sleep to simulate work
+	fmt.Println("Working...")
+	duration := getRandomDuration(1, 5)
+	time.Sleep(duration)
+}
+
+func getRandomDuration(min, max int) time.Duration {
+	// Create a buffer to store the random bytes
+	b := make([]byte, 8)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert the random bytes to an integer
+	randomInt := binary.BigEndian.Uint64(b)
+
+	// Scale the integer to the desired range and convert to a time.Duration
+	randomDuration := min + int(randomInt%(uint64(max-min+1)))
+	return time.Duration(randomDuration) * time.Second
+}
