93dcced09eac4ac3a0f1dfd9518ff62efe37620f
Sean Zheng <blackhorseya@gmail.com>
2024-07-26 12:32:40 +0800

    feat: consolidate Bazel build configuration and define constants
    - Add a new file `BUILD.bazel` for `ton-fullnode`
    - Define `ton-fullnode_lib` and `ton-fullnode` binaries in the `BUILD.bazel` file
    - Add constants for `mainnetConfigPath` and `testnetConfigPath` in `main.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- ton-fullnode/main.go
+++ ton-fullnode/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+const (
+	mainnetConfigPath = "https://ton.org/global.config.json"
+	testnetConfigPath = "https://ton-blockchain.github.io/testnet-global.config.json"
+)
+
 func main() {
 	fmt.Println("Hello, World!")
 }
