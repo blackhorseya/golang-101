3e36f3a49fa03c1093b2b1901629fd07068532d9
Sean Zheng <blackhorseya@gmail.com>
2024-07-26 12:42:07 +0800

    chore: update dependencies and error handling in codebase
    - Update dependencies in MODULE.bazel file
    - Add a new dependency in go.mod file
    - Add two new dependencies in ton-fullnode/BUILD.bazel file
    - Update main.go file to include new dependencies and additional error handling and logging
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- ton-fullnode/main.go
+++ ton-fullnode/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/xssnick/tonutils-go/liteclient"
+	"github.com/xssnick/tonutils-go/ton"
 )
 
 const (
@@ -10,5 +15,26 @@ const (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	client := liteclient.NewConnectionPool()
+
+	err := client.AddConnectionsFromConfigUrl(context.Background(), mainnetConfigPath)
+	if err != nil {
+		log.Fatalln("Failed to add connections from config:", err)
+		return
+	}
+
+	api := ton.NewAPIClient(client)
+	master, err := api.GetMasterchainInfo(context.Background())
+	if err != nil {
+		log.Fatalln("Failed to get masterchain info:", err)
+		return
+	}
+
+	message, err := json.Marshal(master)
+	if err != nil {
+		log.Fatalln("Failed to marshal masterchain info:", err)
+		return
+	}
+
+	log.Println(string(message))
 }
